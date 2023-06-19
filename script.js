3ef61abc533be74c48416c1773d40f5323ea7009
MDTufan <129486543+MDTufan@users.noreply.github.com>
2023-06-19 07:45:59 -0700

    Add files via upload
    javascript.object

---
+++ script.js
@@ -0,0 +1,126 @@
+
+// Function Exmple.......
+
+
+// function FastNumber(num1,num2) {
+//   var num3 = num1+num2;
+//   return num3; 
+
+// }
+
+// var ruselt= FastNumber(20,10);
+// console.log(ruselt);
+
+
+
+
+// function SecondNumber(num4,num5,num6) {
+//   var num7 = num4 + num5;
+//   var num8 = num7 / num6;
+//   return num8; 
+
+// }
+
+// var ruselt= SecondNumber(20,10,3);
+// console.log(ruselt)
+
+
+
+
+
+// switch exmple.............. 
+
+// var digit = parseInt(prompt("Enter a digit : "));
+// switch (digit) {
+//     case 0:
+//         document.write("Zero");
+//         break;
+//   case 0:
+//     document.write("Zero");
+//     break;
+
+//     case 1:
+//         document.write("One");
+//         break;
+//   case 1:
+//     document.write("One");
+//     break;
+
+//     case 2:
+//         document.write("Two");
+//         break;
+//   case 2:
+//     document.write("Two");
+//     break;
+
+//     case 3:
+//         document.write("Three");
+//         break;
+//   case 3:
+//     document.write("Three");
+//     break;
+
+//     case 4:
+//         document.write("Four");
+//         break;
+//   case 4:
+//     document.write("Four");
+//     break;
+
+//     case 5:
+//         document.write("Five");
+//         break;
+//   case 5:
+//     document.write("Five");
+//     break;
+
+//     case 6:
+//         document.write("Six");
+//         break;
+//   case 6:
+//     document.write("Six");
+//     break;
+
+//     case 7:
+//         document.write("Seven");
+//         break;
+//   case 7:
+//     document.write("Seven");
+//     break;
+
+//     case 8:
+//         document.write("Eight");
+//         break;
+//   case 8:
+//     document.write("Eight");
+//     break;
+
+//     case 9:
+//         document.write("Nine");
+//         break;
+//   case 9:
+//     document.write("Nine");
+//     break;
+
+//     default:
+//         document.write("Not a digit");
+// }
+
+
+// objects exmple..............
+  
+var person = {
+  firstName: "Tufan ",
+  lastName: "Ali",
+  age: 23,
+  gpa:4.92,
+};
+
+console.log(person);
+
+
+
+
+
+
+ 
